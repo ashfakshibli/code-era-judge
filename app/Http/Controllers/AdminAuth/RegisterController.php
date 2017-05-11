b2c495930a5bef69c39ce626e44eda2868f4eb25
shibli16 <emon744254>
2017-05-11 23:20:20 +0600

    Problem to get data in database for eadmin

---
+++ app/Http/Controllers/AdminAuth/RegisterController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\AdminAuth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Support\Facades\Validator;
+
+use App\Admin;
+use Auth;
+//use Illuminate\Foundation\Auth\RegistersUsers;
+
+
+
+
+class RegisterController extends Controller
+{
+	protected $redirectPath = 'admin_home';
+
+    public function showRegistrationForm()
+    {
+    	return view('admin.auth.register');
+    }
+
+
+    public function register(Request $request)
+    {
+    	//validation
+    	
+    	dd($this->validator($request->all())->validate());
+
+
+    	//create admin
+    	$admin = $this->create($request->all());
+
+
+    	//authenticates admin
+    	$this->guard()->login($admin);
+
+    	//redirect
+    	return redirect($this->redirectPath);
+    }
+
+    public function validator(array $data)			
+    {
+    	return Validator::make($data, [
+    			'name' => 'required|max:255',
+    			'email' => 'required|email|max:255|unique:admins',
+    			'pasword' => 'required|min:6|confirmed',
+    		]);
+    }
+
+    public function create(array $data)
+    {
+    	return Admin::create([
+    		'name' => $data['name'],
+    		'email' => $data['email'],
+    		'password' => bcrypt($data['password']), 
+    		]);
+    }
+
+
+    public function guard()
+    {
+    	return Auth::guard('admin');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,21 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+
+//Admin Routes
+Route::get('/admin_register', 'AdminAuth\RegisterController@showRegistrationForm')->name('admin_register');
+Route::post('/admin_register', 'AdminAuth\RegisterController@register')->name('admin_store');
+
+Route::get('/admin_login', 'AdminAuth\LoginController@showLoginForm')->name('admin_login');
+
+
+
+Route::get('/admin_home', function(){
+	return view('admin.home');
+});
